8364c33b0789f337f940609e0492ba078d198aad
fc2727 <aaaasdfd1234@gmail.com>
2022-10-10 01:32:44 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by testing on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
